073de3ef3746bc730f146129be2ddab8561ce31e
MARY JULIA SHARINA MALAGAYO <150454683+iyaqn@users.noreply.github.com>
2024-02-19 05:16:38 +0000

    .

--- src/Books.js
+++ src/Books.js
@@ -2,20 +2,45 @@ import "./App.css";
 
 function Books(props) {
   return (
-    <div className="books" >
-      <div className="containerr" >
-        <a href="">
-          <img className="imagee" src={props.booksObj.photoName} alt={props.name} />
+    <div className="books">
+      <div className="containerr">
+        {props.booksObj.soldOut === true ? (
+          <img
+            className="sold-out"
+            src={props.booksObj.photoName}
+            alt={props.name}
+          />
+        ) : (
+          <a href="">
+            <img
+              className="imagee"
+              src={props.booksObj.photoName}
+              alt={props.name}
+            />
+          </a>
+        )}
+
+        {props.booksObj.soldOut === false ? (
           <div className="overlay">
             <img className="addImage" src="./add.png" alt="Add" />
           </div>
-        </a>
+        ) : null}
       </div>
+
       <div>
-        <a href="">
-          <h5>{props.booksObj.name}</h5>
-        </a>
-        <span>{props.booksObj.price}</span>
+        {props.booksObj.soldOut === false ? (
+          <a href="">
+            <h5>{props.booksObj.name}</h5>
+          </a>
+        ) : (
+          <h5 className="sold-out">{props.booksObj.name}</h5>
+        )}
+
+        {props.booksObj.soldOut === false ? (
+          <span>{props.booksObj.price}</span>
+        ) : (
+          <span className="sold-out">{props.booksObj.price} - Sold Out</span>
+        )}
       </div>
     </div>
   );
